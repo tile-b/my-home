6f6bb375e04c3ad2d0b0e140aa551aabeedcc100
tile-b <dejantica.b@gmail.com>
2025-01-08 13:12:11 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div style={{paddingBottom: '10px'}}>
+          Šamac
+</div>
+<div class="spinnerContainer">
+  <div class="loader">
+    Uskoro
+    <div class="words">
+      <span class="word">Zavjese</span>
+      <span class="word">Komarnici</span>
+      <span class="word">PVC Stolarija</span>
+      <span class="word">Tepisi</span>
+      <span class="word">Staze</span>
+      <span class="word">Zavjesni Paravani</span>
+      <span class="word">Ograde</span>
+      <span class="word">Zaluzine</span>
     </div>
+  </div>
+</div>
+
+
+      </header>
+
+</div>
+
   );
 }
 
